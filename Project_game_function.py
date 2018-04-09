9e6e2ddc129a3089db1fe98a5acbca83255c4165
Oleslav <oleslav.boychuk@gmail.com>
2018-04-09 21:55:08 +0300

    change_one
    Minor changes to game

---
+++ Project_game_function.py
@@ -0,0 +1,32 @@
+import pygame,sys
+def check_events(hero):
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, hero)
+        elif event.type == pygame.KEYUP:
+            chek_keyup_events(event, hero)
+def update_screen(background,hero):
+    #background.update()#
+    background.update()
+    hero.blitme()
+    pygame.display.flip()
+def check_keydown_events(event,hero):
+    if event.key == pygame.K_RIGHT:
+        hero.moving_right = True
+    if event.key == pygame.K_LEFT:
+        hero.moving_left = True
+    if event.key == pygame.K_UP:
+        hero.moving_up = True
+    if event.key == pygame.K_DOWN:
+        hero.moving_down = True
+def chek_keyup_events(event,hero):
+    if event.key == pygame.K_RIGHT:
+        hero.moving_right = False
+    if event.key == pygame.K_LEFT:
+        hero.moving_left = False
+    if event.key == pygame.K_UP:
+        hero.moving_up = False
+    if event.key == pygame.K_DOWN:
+        hero.moving_down = False
---
+++ Project_main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+from Project_settings import Settings
+from Project_hero import Hero
+from Project_background import Background
+import Project_game_function
+def init_game():
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+    pygame.display.set_caption("Project")
+    hero = Hero(screen)
+    background = Background(screen)
+
+    while True:
+
+        Project_game_function.check_events(hero)
+        hero.update()
+        Project_game_function.update_screen(background, hero)
+
+init_game()
